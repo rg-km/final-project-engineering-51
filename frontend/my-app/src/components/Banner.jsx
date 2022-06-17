de3850618c64aa96531658b45e36e03a20d6bb6c
Zena Dwi Mentari <zenadwimentari@gmail.com>
2022-06-17 23:08:04 +0700

    footer

--- frontend/my-app/src/components/Banner.jsx
+++ frontend/my-app/src/components/Banner.jsx
@@ -3,28 +3,33 @@ import {
     Image,
     Button,
     Text,
+    Input,
     Container,
     Heading,
-    Grid,
-    Badge,
-    FormControl,
-    Input,
-} from '@chakra-ui/react';
+    ButtonGroup,
+    Divider,
+    IconButton,
+    Stack,
+}
+from '@chakra-ui/react';
+import { PhoneIcon, ChevronRightIcon, AtSignIcon} from '@chakra-ui/icons';
 import illustration from '../assets/illustration.png';
+import * as React from 'react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 function Banner(){
     return(
     <>
         <header>
             <Box boxShadow='2xl' >
-                <Box display="flex" alignItems="center" justifyContent="space-between" ml={200} mr={200}>
+                <Box display="flex" alignItems="center" justifyContent="space-between" ml={70} mr={70}>
                     <Box>
                         <Text fontWeight="bold" color="#C73661" fontSize='5xl'>KenaliAku</Text>
                     </Box>
                     <Box display="flex">
-                        <Text pr={5} fontSize='2xl'>Beranda</Text>
-                        <Text pr={5} fontSize='2xl'>Layanan</Text>
-                        <Text pr={5} fontSize='2xl'>Tentang Kami</Text>
+                        <Button mr={5} colorScheme="gray.600" fontSize="1xl" variant='link'>Beranda</Button>
+                        <Button mr={5} colorScheme="gray.600" fontSize="1xl" variant='link'>Layanan</Button>
+                        <Button mr={5} colorScheme="gray.600" fontSize="1xl" variant='link'>Tentang Kami</Button>
                         <Button mr={5} colorScheme="gray.600" fontSize="1xl" color="#E84B6A" variant='outline'>
                             Daftar
                         </Button>
@@ -36,28 +41,92 @@ function Banner(){
             </Box>
         </header>
         <Box>
-            {/* main punch text */}
-            <Container maxW="container.xl">
-            <Box display="flex" alignItems="center" py="20">
-                <Box mr="6">
-                <Heading as="h1" size="2xl">
-                    <Box fontWeight="black">
-                    Yuk, Kenali Minat dan Bakatmu!!
+            <Container maxWidth="container.xl">
+                <Box display="flex" alignItems="center" py="20" flexDirection="row">
+                    <Box mr="6">
+                        <Heading as="h1" size="2xl">
+                            <Box fontWeight="black">
+                            Yuk, Kenali Minat dan Bakatmu!!
+                            </Box>
+                        </Heading>
+                        <Box mt="6" fontWeight="medium">
+                        Kenali dirimu dengan melakukan Tes Minat dan Bakat di KenaliAku. Temukan jurusan studi terbaik untuk #MasaDepanmu
+                        </Box>
+                        <Box mt="4">
+                            <Button pr={5} fontSize="lg" color="black" variant='outline' background="#FFCD1D">
+                                Ikuti Tes Sekarang
+                            </Button>
+                        </Box>
+                        </Box>
+                        <Box w="100%">
+                        <Image w="100%" src={illustration} alt="illustration" />
                     </Box>
-                </Heading>
-                <Box mt="6" fontWeight="medium">
-                Kenali dirimu dengan melakukan Tes Minat dan Bakat di KenaliAku. Temukan jurusan studi terbaik untuk #MasaDepanmu
                 </Box>
-                    <Button pr={5} fontSize="lg" color="#E84B6A" variant='outline'>
-                        Ikuti Tes Sekarang
-                    </Button>
-                </Box>
-                <Box w="100%">
-                <Image w="100%" src={illustration} alt="illustration" />
-                </Box>
-            </Box>
             </Container>
         </Box>
+        <footer>
+            <Box background="#FFF0F5">
+            <Box ml={70} mr={70}>
+            <Stack
+            spacing="8"
+            direction={{ base: 'column', md: 'row' }}
+            justify="space-between"
+            py={{ base: '12', md: '10' }}
+            >
+            <Stack spacing={{ base: '6', md: '8' }} align="start">
+                <Text fontWeight="bold" fontSize='3xl'>KenaliAku</Text>
+            </Stack>
+            <Stack
+                direction={{ base: 'column-reverse', md: 'column', lg: 'row' }}
+                spacing={{ base: '12', md: '8' }}
+            >
+                <Stack direction="row" spacing="10">
+                    <Stack spacing="4" minWidth="200" flex="1" >
+                        <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                        Kontak Kami
+                        </Text>
+                        <Stack spacing="3" shouldWrapChildren>
+                        <Text fontSize="sm"><PhoneIcon/> 087748238383</Text>
+                        <Text fontSize="sm"><AtSignIcon/> KenaliAku@gmail.com</Text>
+                        </Stack>
+                    </Stack>
+                    <Stack spacing="4" minW="36" flex="1">
+                        <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                        Menu
+                        </Text>
+                        <Stack spacing="3" shouldWrapChildren>
+                        <Button variant="link" color="black" fontSize="sm"><ChevronRightIcon/> layanan</Button>
+                        <Button variant="link" color="black" fontSize="sm"><ChevronRightIcon/> Tentang</Button>
+                        </Stack>
+                    </Stack>
+                    <Stack spacing="4">
+                    <Text fontSize="sm" fontWeight="semibold" color="subtle">Ikuti Kami</Text>
+                    <ButtonGroup variant="ghost">
+                        <IconButton as="a" href="#" aria-label="LinkedIn" icon={<FaLinkedin fontSize="1.25rem" />} />
+                        <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
+                        <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="1.25rem" />} />
+                    </ButtonGroup>
+                    </Stack>
+                </Stack>
+            </Stack>
+            </Stack>
+            </Box>
+            <Divider borderColor="red.200"/>
+            <Box ml={70} mr={70}>
+            <Stack
+            pt="4"
+            pb="4"
+            justify="space-between"
+            direction={{ base: 'column-reverse', md: 'row' }}
+            align="center"
+            >
+            <Text fontWeight="bold" fontSize="sm" color="subtle">
+                &copy; {new Date().getFullYear()} Kenali Aku. All rights reserved.
+            </Text>
+            </Stack>
+            </Box>
+            </Box>
+        </footer>
     </>
     );
 }
