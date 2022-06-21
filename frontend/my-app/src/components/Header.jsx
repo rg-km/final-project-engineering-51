e8d44f22aa9f18f2012fabd4358e6bdbabe9a267
Zena Dwi Mentari <zenadwimentari@gmail.com>
2022-06-21 10:29:35 +0700

    update header

--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -10,16 +10,16 @@ import * as React from 'react';
 function Header(){
     return(
     <>
-        <header>
-            <Box boxShadow='2xl' >
+        <header as="header" position="fixed">
+            <Box boxShadow='2xl' as="header" position="fixed" w="100%" backgroundColor="white">
                 <Box display="flex" alignItems="center" justifyContent="space-between" ml={70} mr={70}>
                     <Box>
                         <Text fontWeight="bold" color="#C73661" fontSize='5xl'>KenaliAku</Text>
                     </Box>
                     <Box display="flex" alignItems="center">
-                        <Link mr={5} colorScheme="gray.600" fontSize="1xl" >Beranda</Link>
-                        <Link mr={5} colorScheme="gray.600" fontSize="1xl" >Tentang</Link>
-                        <Link mr={5} colorScheme="gray.600" fontSize="1xl" >Layanan</Link>
+                        <Link href="#banner" mr={5} colorScheme="gray.600" fontSize="1xl" >Beranda</Link>
+                        <Link href="#about" mr={5} colorScheme="gray.600" fontSize="1xl" >Tentang</Link>
+                        <Link href="#service" mr={5} colorScheme="gray.600" fontSize="1xl" >Layanan</Link>
                         <Button mr={5} colorScheme="gray.600" fontSize="1xl" color="#E84B6A" variant='outline'>
                             Daftar
                         </Button>
